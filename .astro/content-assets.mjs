f2a576434bdd01a0f129b37287a070897fc4d394
ezra neer <ezra@neerautomata.com>
2024-12-29 16:31:09 -0800

    images for cards for posts

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_Z1pJkjT from "../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fget-in-the-gundam.md";
+export default new Map([["../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fget-in-the-gundam.md", __ASTRO_IMAGE_IMPORT_Z1pJkjT]]);
+		
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,19 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
     loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "src/data/blog" }),
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
-        description: z.string().optional(),
+        cover: image().optional(),
+        coverAlt: z.string().optional(),
         pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
-        author: z.string().optional()
+        author: z.string().optional(), 
+        excerpt: z.string(), 
     })
 })
 
-export const collections = { blog, }
+export const collections = { 
+    blog: blogCollection,
+ }
