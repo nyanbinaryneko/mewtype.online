b33cf6ac1ca908e7d266d35001017cd4d94ea72e
ezra neer <ezra@neerautomata.com>
2024-12-30 11:33:25 -0800

    unfucked mdx support

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_Z1pJkjT from "../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fget-in-the-gundam.md";
-export default new Map([["../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fget-in-the-gundam.md", __ASTRO_IMAGE_IMPORT_Z1pJkjT]]);
+import __ASTRO_IMAGE_IMPORT_Z6O2ta from "../../assets/images/hell.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Ftest.mdx";
+import __ASTRO_IMAGE_IMPORT_Z15n72v from "../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fsdfaadsfds.mdx";
+export default new Map([["../../assets/images/hell.jpg?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Ftest.mdx", __ASTRO_IMAGE_IMPORT_Z6O2ta], ["../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Fsdfaadsfds.mdx", __ASTRO_IMAGE_IMPORT_Z15n72v]]);
 		
--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,5 @@
-export default new Map();
+
+export default new Map([
+["src/data/blog/test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Ftest.mdx&astroContentModuleFlag=true")],
+["src/data/blog/sdfaadsfds.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fsdfaadsfds.mdx&astroContentModuleFlag=true")]]);
+		
