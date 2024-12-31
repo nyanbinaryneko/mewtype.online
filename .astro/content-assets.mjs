bc409cc2abf633c3e7f11a926c4d2667cb771044
ezra neer <ezra@neerautomata.com>
2024-12-31 15:27:53 -0800

    .astro not needed

--- .astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_ZAK4rT from "../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Ftag-test-2.mdx";
-import __ASTRO_IMAGE_IMPORT_UrNIP from "../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Ftag-test.mdx";
-export default new Map([["../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Ftag-test-2.mdx", __ASTRO_IMAGE_IMPORT_ZAK4rT], ["../../assets/images/screenshot-2023-07-25-220908.png?astroContentImageFlag=&importer=src%2Fdata%2Fblog%2Ftag-test.mdx", __ASTRO_IMAGE_IMPORT_UrNIP]]);
-		
--- .astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/data/blog/tag-test-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Ftag-test-2.mdx&astroContentModuleFlag=true")],
-["src/data/blog/tag-test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Ftag-test.mdx&astroContentModuleFlag=true")]]);
-		
