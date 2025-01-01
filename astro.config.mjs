6db7a46d95d669180b890c4f6b8acc596ca2dfc8
ezra neer <ezra@neerautomata.com>
2024-12-31 16:28:12 -0800

    fix cors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from 'astro-icon'
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    }
+  },
+  adapter: netlify(),
   compressHTML: true,
   integrations: [
     mdx(),
